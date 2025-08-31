ec3d7758cb92963f88569403416571a6570e849f
Garrett Kilgore <garrettkilgore@DESKTOP-4P8F6C4.localdomain>
2025-08-30 21:31:22 -0600

    first commit

--- calc.py
+++ calc.py
@@ -1,7 +1,25 @@
-class Calc:
+import unittest
 
+class Calc:
     def add(self, a, b):
         return a + b
 
     def mul(self, a, b):
         return a * b
+
+class TestCalc(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calc()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_mul(self):
+        self.assertEqual(self.calc.mul(2, 3), 6)
+        self.assertEqual(self.calc.mul(-1, 1), -1)
+        self.assertEqual(self.calc.mul(0, 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()
--- circle.py
+++ circle.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 class Circle:
 
@@ -24,3 +25,46 @@ class Circle:
 
     def getCircumference(self):
         return 2. * math.pi * self.mRadius
+
+
+class testing(unittest.TestCase):
+
+    def setUp(self):
+        self.circle = Circle(3)
+
+    def test_init__(self):
+        circle = Circle(7)
+        self.assertEqual(circle.mRadius, 7)
+
+    def test_getRadius(self):
+        self.assertEqual(self.circle.getRadius(), 3)
+
+    def test_setRadius_positive(self):
+        circle = Circle(1)
+        radius = circle.setRadius(3)
+        self.assertTrue(radius)
+        self.assertEqual(circle.mRadius, 3)
+
+    def test_set_radius_negative(self):
+        circle = Circle(-1)
+        radius = circle.setRadius(-3)
+        self.assertFalse(radius)
+        self.assertEqual(circle.mRadius, -1)
+
+    def test_area(self):
+        circle = Circle(3)
+        expected = math.pi * 3 * 3
+        self.assertAlmostEqual(circle.getArea(), expected, places = 5)
+
+    def test_areaIf2(self):
+        circle = Circle(2)
+        self.assertEqual(circle.getArea(), 0)
+
+    def test_getCircumference(self):
+        circle = Circle(3)
+        expected = 2 * math.pi * 3
+        self.assertAlmostEqual(circle.getCircumference(), expected, places = 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
